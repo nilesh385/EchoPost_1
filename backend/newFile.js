6a12f3fc53323c588372a4bd12a60d1e6ed2a5bf
Nilesh Kumar <nileshandnilesh120@gmail.com>
2024-05-03 21:35:48 +0530

    removed useless files

--- backend/newFile.js
+++
@@ -1,5 +0,0 @@
-import bodyParser from "body-parser";
-import { app } from "./server.js";
-
-// app.use(express.json()); //to parse JSOn data in the req.body
-app.use(bodyParser.json()); //to parse JSOn data in the req.body

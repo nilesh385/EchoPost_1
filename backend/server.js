a24330b87d4a23322f73fd4858ed29b60a384ff2
Nilesh Kumar <nileshandnilesh120@gmail.com>
2024-05-04 11:45:45 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,15 @@ import postRoutes from "./routes/post.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
+import path from "path";
+
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 //connect to cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -29,6 +32,18 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
+//http://localhost:5000 => backend,frontend
+
+if (process.env.NODE_ENV === "production") {
+  console.log("running in production mode");
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // react app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 //listening server (it's http server created in socket file) on port or 3000
 server.listen(PORT, () => {
